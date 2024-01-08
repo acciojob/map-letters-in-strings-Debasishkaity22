c6ae3c0d36f22e90005ef3806d17e3df0f698db8
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2024-01-08 21:55:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function mapLetters(str){
+	let ans={};
+for(let i=0;i<str.length;i++){
+    let char=str[i];
+    if(!ans[char]){
+      ans[char]=[i];
+    }
+    else{
+      ans[char].push(i);
+    }
+     
+}
+	return ans;
+}
+let input=prompt("Enter Word :");
+alert(mapLetters(input));
